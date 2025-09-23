62a00ab9224e62160f4e082f7428530455333d82
root-kjs <root.kjs82@gmail.com>
2025-09-23 19:01:22 +0900

    주석처리  //schedule(25, loc::schedulLocationList2); // [강사2025-09-11]스케줄링을 사용하지 않는다.

--- src/main/java/user/api/ApiRun.java
+++ src/main/java/user/api/ApiRun.java
@@ -29,11 +29,11 @@ public class ApiRun {
 
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterReady() {
-         schedule(0, area::schedulLdongCode2depth1);
-         schedule(3, area::schedulAreaList2LDong);
-         schedule(20, lcls::schedulLclsSystmCode2);
-         schedule(25, ldong::schedulLdongCode2);
-         schedule(25, loc::schedulLocationList2); // [강사2025-09-11]스케줄링을 사용하지 않는다.
+        schedule(0, area::schedulLdongCode2depth1);
+        schedule(3, area::schedulAreaList2LDong);
+        schedule(20, lcls::schedulLclsSystmCode2);
+        schedule(25, ldong::schedulLdongCode2);
+        //schedule(25, loc::schedulLocationList2); // [강사2025-09-11]스케줄링을 사용하지 않는다.
     }
 
     private void schedule(int delaySec, Runnable task) {
